3b8585c76edb2fbcec9dd1f6ccb50530f3721cf2
JustineDaveMagnaye <157860693+JustineDaveMagnaye@users.noreply.github.com>
2024-09-25 21:41:14 +0800

    Update EmployeeQueryConstants.java

--- rc-2fa/src/main/java/com/two/factor/authentication/data/utils/EmployeeQueryConstants.java
+++ rc-2fa/src/main/java/com/two/factor/authentication/data/utils/EmployeeQueryConstants.java
@@ -5,26 +5,26 @@ public final class EmployeeQueryConstants {
     /**
      * SQL query to retrieves all employees from the database.
      */
-    public static final String GET_ALL_EMPLOYEE_STATEMENT = "SELECT * FROM EMPLOYEES";
+    public static final String GET_ALL_EMPLOYEE_STATEMENT = "SELECT * FROM EMPLOYEE";
 
     /**
      * SQL query to retrieves an EMPLOYEES by their EMPLOYEES Number from the database.
      */
-    public static final String GET_EMPLOYEE_BY_ID_STATEMENT = "SELECT * FROM EMPLOYEES WHERE EMPLOYEE_NO = ?";
+    public static final String GET_EMPLOYEE_BY_ID_STATEMENT = "SELECT * FROM EMPLOYEE WHERE EMPLOYEE_NO = ?";
 
-    public static final String GET_BY_ID_STATEMENT = "SELECT * FROM EMPLOYEES WHERE ID = ?";
+    public static final String GET_BY_ID_STATEMENT = "SELECT * FROM EMPLOYEE WHERE ID = ?";
 
     /**
      * SQL query that adds a new EMPLOYEES to the database.
      */
-    public static final String ADD_EMPLOYEE_STATEMENT = "INSERT INTO EMPLOYEES (LAST_NAME, FIRST_NAME, MIDDLE_NAME, POSITION_IN_RC, DATE_EMPLOYED, " +
+    public static final String ADD_EMPLOYEE_STATEMENT = "INSERT INTO EMPLOYEE (LAST_NAME, FIRST_NAME, MIDDLE_NAME, POSITION_IN_RC, DATE_EMPLOYED, " +
             "BIRTHDATE, BIRTHPLACE, SEX, ADDRESS, CONTACT_NUMBER, CIVIL_STATUS, CITIZENSHIP, RELIGION, EMAIL, " +
             "SSS_NO, TIN_NO, PAGIBIG_NO, STATION_ID, EMPLOYEE_NUMBER) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     /**
      * SQL query to updates an existing EMPLOYEES in the database.
      */
-    public static final String UPDATE_STATEMENT = "UPDATE EMPLOYEES SET LAST_NAME = ?, FIRST_NAME = ?, MIDDLE_NAME = ?, POSITION_IN_RC = ?, DATE_EMPLOYED = ?, " +
+    public static final String UPDATE_STATEMENT = "UPDATE EMPLOYEE SET LAST_NAME = ?, FIRST_NAME = ?, MIDDLE_NAME = ?, POSITION_IN_RC = ?, DATE_EMPLOYED = ?, " +
             "BIRTHDATE = ?, BIRTHPLACE = ?, SEX = ?, ADDRESS = ?, CONTACT_NUMBER = ?, CIVIL_STATUS = ?, CITIZENSHIP = ?, RELIGION = ?, EMAIL = ?, " +
             "SSS_NO = ?, TIN_NO = ?, PAGIBIG_NO = ?, STATION_ID = ? WHERE EMPLOYEE_NO = ?";
 }
